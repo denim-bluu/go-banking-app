fbe62468fbeb96e5d8b6750a36427cd87bcba63a
Joon Kang <denim.bluu@gmail.com>
2023-10-22 20:17:23 +0100

    feat: Impelement database transaction

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 )
 
+// Store provides all functions to execute db quries and transactions
 type Store struct {
 	*Queries
 	db *sql.DB
@@ -13,11 +14,12 @@ type Store struct {
 
 func NewStore(db *sql.DB) *Store {
 	return &Store{
-		db:      db,
 		Queries: New(db),
+		db:      db,
 	}
 }
 
+// execTx executes a function within a database transaction
 func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
 	if err != nil {
@@ -34,30 +36,53 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	return tx.Commit()
 }
 
+// Contains the input arguments of the transfer transaction
 type TransferTxParams struct {
-	FromAccoubtID int64 `json:"from_account_id"`
+	FromAccountID int64 `json:"from_account_id"`
 	ToAccountID   int64 `json:"to_account_id"`
 	Amount        int64 `json:"amount"`
 }
 
+// The result of transfer transaction
 type TransferTxResult struct {
-	Transfer      int64 `json:"transfer"`
-	FromAccoubtID int64 `json:"from_account_id"`
-	ToAccountID   int64 `json:"to_account_id"`
-	FromEntry     int64 `json:"from_entry"`
-	ToEntry       int64 `json:"to_entry"`
+	Transfer      Transfer `json:"transfer"`
+	FromAccoubtID Account  `json:"from_account_id"`
+	ToAccountID   Account  `json:"to_account_id"`
+	FromEntry     Entry    `json:"from_entry"`
+	ToEntry       Entry    `json:"to_entry"`
 }
 
-// func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
-// 	var result TransferTxResult
+// TransferTx performs a money transfer from one account to another
+// It creates a transfer record, add account entries, and update accounts balance within a single transaction
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
 
-// 	err := store.execTx(ctx, func(q *Queries) error {
-// 		var err error
-// 		result.Transfer, err := q.CreateTrasnfer(ctx, CreateTransferParams{
-// 			FromAccountID: arg.FromAccoubtID,
-// 			ToAccountID:   arg.ToAccountID,
-// 			Amount:        arg.Amount,
-// 		},
-// 		)
-// 	})
-// }
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	return result, err
+}
